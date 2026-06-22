b24bf2c1e787715c116e6da08087f0e3f755697d
mohsen848esf <mohsenesfandiari848@gmail.com>
2026-06-22 10:00:00 +0330

    feat: add useWatchlist hook with localStorage persistence

--- src/constants/storage.ts
+++ src/constants/storage.ts
@@ -2,4 +2,5 @@ export const STORAGE_KEYS = {
   selectedSymbol: 'tradelite-selected-symbol',
   chartInterval: 'tradelite-chart-interval',
   priceAlerts: 'tradelite-price-alerts',
+  watchlist: 'tradelite-watchlist',
 } as const
